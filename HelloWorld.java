73385badd7e49157f12a7498ea6d3306c8571e63
formation167 <formation167@gail.com>
2021-05-12 11:44:53 +0200

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
+
+// Compiler 
+// Tester
+// Créer un repo git en local
+// Ignorer le fichier .class
+// Créez un commit avec un message "Initial commit"
+// Créer un repo distant : hello-maven
+// Pusher le code dans le repo distant
